6fbd87b32ad9e36fb18501ecd582a0eba26de004
David Mandelberg <david@mandelberg.org>
2025-06-09 14:38:14 -0400

    Start on config file parsing

---
+++ ginjarator/config.py
@@ -0,0 +1,46 @@
+# Copyright 2025 David Mandelberg
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Config file."""
+
+from collections.abc import Collection
+import dataclasses
+import pathlib
+from typing import Any, Self
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class Config:
+    """Config.
+
+    Attributes:
+        source_paths: Source files/directories, relative to the config file.
+        build_paths: Build files/directories, relative to the config file.
+    """
+
+    source_paths: Collection[pathlib.Path]
+    build_paths: Collection[pathlib.Path]
+
+    @classmethod
+    def parse(cls, raw: Any, /) -> Self:
+        """Returns the parsed config from toml data."""
+        if unexpected_keys := raw.keys() - {"source_paths", "build_paths"}:
+            raise ValueError(f"Unexpected keys: {list(unexpected_keys)}")
+        return cls(
+            source_paths=tuple(
+                map(pathlib.Path, raw.get("source_paths", ["src"]))
+            ),
+            build_paths=tuple(
+                map(pathlib.Path, raw.get("build_paths", ["build"]))
+            ),
+        )
---
+++ ginjarator/config_test.py
@@ -0,0 +1,53 @@
+# Copyright 2025 David Mandelberg
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=missing-module-docstring
+
+import pathlib
+from typing import Any
+
+import pytest
+
+from ginjarator import config
+
+
+def test_config_parse_error() -> None:
+    with pytest.raises(ValueError, match="kumquat"):
+        config.Config.parse(dict(kumquat="foo"))
+
+
+@pytest.mark.parametrize(
+    "raw,expected",
+    (
+        (
+            {},
+            config.Config(
+                source_paths=(pathlib.Path("src"),),
+                build_paths=(pathlib.Path("build"),),
+            ),
+        ),
+        (
+            dict(
+                source_paths=["src1", "src2"],
+                build_paths=["build1", "build2"],
+            ),
+            config.Config(
+                source_paths=(pathlib.Path("src1"), pathlib.Path("src2")),
+                build_paths=(pathlib.Path("build1"), pathlib.Path("build2")),
+            ),
+        ),
+    ),
+)
+def test_config_parse(raw: Any, expected: config.Config) -> None:
+    assert config.Config.parse(raw) == expected
