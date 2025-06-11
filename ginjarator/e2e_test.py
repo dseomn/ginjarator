b03b266571da986c0f79fd9d006e77c825d7f1a0
David Mandelberg <david@mandelberg.org>
2025-06-11 18:09:41 -0400

    Add tests that make changes and re-run ninja

--- ginjarator/e2e_test.py
+++ ginjarator/e2e_test.py
@@ -158,3 +158,227 @@ def test_conflicting_writes() -> None:
 
     _run_init()
     _run(("ninja",), expect_success=False)
+
+
+def test_add_template() -> None:
+    _run_init()
+    _run_ninja()
+
+    pathlib.Path("ginjarator.toml").write_text(
+        textwrap.dedent(
+            """\
+            templates = [
+                "src/foo.jinja",
+            ]
+            """
+        )
+    )
+    pathlib.Path("src/foo.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% do ginjarator.fs.write_text("build/out", "contents") %}
+            """
+        )
+    )
+
+    _run_ninja()
+
+    assert pathlib.Path("build/out").read_text() == "contents"
+
+
+def test_add_template_dependency() -> None:
+    pathlib.Path("ginjarator.toml").write_text(
+        textwrap.dedent(
+            """\
+            templates = [
+                "src/template-1.jinja",
+            ]
+            """
+        )
+    )
+    pathlib.Path("src/template-1.jinja").write_text("")
+
+    _run_init()
+    _run_ninja()
+
+    pathlib.Path("ginjarator.toml").write_text(
+        textwrap.dedent(
+            """\
+            templates = [
+                "src/template-1.jinja",
+                "src/template-2.jinja",
+            ]
+            """
+        )
+    )
+    pathlib.Path("src/template-1.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% set out_2 = ginjarator.fs.read_text("build/out-2") %}
+            {% if out_2 is none %}
+                {% do ginjarator.fs.add_output("build/out-1") %}
+            {% else %}
+                {% do ginjarator.fs.write_text(
+                    "build/out-1",
+                    out_2.replace("2", "1"),
+                ) %}
+            {% endif %}
+            """
+        )
+    )
+    pathlib.Path("src/template-2.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% do ginjarator.fs.write_text("build/out-2", "contents-2") %}
+            """
+        )
+    )
+
+    _run_ninja()
+
+    assert pathlib.Path("build/out-1").read_text() == "contents-1"
+    assert pathlib.Path("build/out-2").read_text() == "contents-2"
+
+
+def test_remove_template() -> None:
+    pathlib.Path("ginjarator.toml").write_text(
+        textwrap.dedent(
+            """\
+            templates = [
+                "src/foo.jinja",
+            ]
+            """
+        )
+    )
+    pathlib.Path("src/foo.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% do ginjarator.fs.write_text("build/out", "contents") %}
+            """
+        )
+    )
+
+    _run_init()
+    _run_ninja()
+
+    pathlib.Path("ginjarator.toml").write_text("")
+    pathlib.Path("src/foo.jinja").unlink()
+
+    _run_ninja()
+
+    assert not pathlib.Path("build/out").exists()
+
+
+def test_remove_template_dependency() -> None:
+    pathlib.Path("ginjarator.toml").write_text(
+        textwrap.dedent(
+            """\
+            templates = [
+                "src/template-1.jinja",
+                "src/template-2.jinja",
+            ]
+            """
+        )
+    )
+    pathlib.Path("src/template-1.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% set out_2 = ginjarator.fs.read_text("build/out-2") %}
+            {% if out_2 is none %}
+                {% do ginjarator.fs.add_output("build/out-1") %}
+            {% else %}
+                {% do ginjarator.fs.write_text(
+                    "build/out-1",
+                    out_2.replace("2", "1"),
+                ) %}
+            {% endif %}
+            """
+        )
+    )
+    pathlib.Path("src/template-2.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% do ginjarator.fs.write_text("build/out-2", "contents-2") %}
+            """
+        )
+    )
+
+    _run_init()
+    _run_ninja()
+
+    pathlib.Path("ginjarator.toml").write_text(
+        textwrap.dedent(
+            """\
+            templates = [
+                "src/template-1.jinja",
+            ]
+            """
+        )
+    )
+    pathlib.Path("src/template-1.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% do ginjarator.fs.write_text("build/out-1", "new-contents-1") %}
+            """
+        )
+    )
+    pathlib.Path("src/template-2.jinja").unlink()
+
+    _run_ninja()
+
+    assert pathlib.Path("build/out-1").read_text() == "new-contents-1"
+    assert not pathlib.Path("build/out-2").exists()
+
+
+def test_template_dependency_changes_creator() -> None:
+    pathlib.Path("ginjarator.toml").write_text(
+        textwrap.dedent(
+            """\
+            templates = [
+                "src/template-1.jinja",
+                "src/template-2.jinja",
+                "src/template-3.jinja",
+            ]
+            """
+        )
+    )
+    pathlib.Path("src/template-1.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% set in_1 = ginjarator.fs.read_text("build/in-1") %}
+            {% if in_1 is none %}
+                {% do ginjarator.fs.add_output("build/out-1") %}
+            {% else %}
+                {% do ginjarator.fs.write_text(
+                    "build/out-1",
+                    in_1.replace("in", "out"),
+                ) %}
+            {% endif %}
+            """
+        )
+    )
+    pathlib.Path("src/template-2.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% do ginjarator.fs.write_text("build/in-1", "contents-2-in") %}
+            """
+        )
+    )
+    pathlib.Path("src/template-3.jinja").write_text("")
+
+    _run_init()
+    _run_ninja()
+
+    pathlib.Path("src/template-2.jinja").write_text("")
+    pathlib.Path("src/template-3.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% do ginjarator.fs.write_text("build/in-1", "contents-3-in") %}
+            """
+        )
+    )
+
+    _run_ninja()
+
+    assert pathlib.Path("build/in-1").read_text() == "contents-3-in"
+    assert pathlib.Path("build/out-1").read_text() == "contents-3-out"
