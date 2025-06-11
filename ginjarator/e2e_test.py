e545f29aeebcd9738cc27a897de7bb3b65b28933
David Mandelberg <david@mandelberg.org>
2025-06-11 18:25:20 -0400

    Add test for a missing dependency

--- ginjarator/e2e_test.py
+++ ginjarator/e2e_test.py
@@ -146,6 +146,36 @@ def test_template_dependencies() -> None:
     assert pathlib.Path("build/out-2").read_text() == "contents-2"
 
 
+def test_template_missing_dependency() -> None:
+    pathlib.Path("ginjarator.toml").write_text(
+        textwrap.dedent(
+            """\
+            templates = [
+                "src/template-2.jinja",
+            ]
+            """
+        )
+    )
+    pathlib.Path("src/template-2.jinja").write_text(
+        textwrap.dedent(
+            """\
+            {% set out_1 = ginjarator.fs.read_text("build/out-1") %}
+            {% if out_1 is none %}
+                {% do ginjarator.fs.add_output("build/out-2") %}
+            {% else %}
+                {% do ginjarator.fs.write_text(
+                    "build/out-2",
+                    out_1.replace("1", "2"),
+                ) %}
+            {% endif %}
+            """
+        )
+    )
+
+    _run_init()
+    _run(("ninja",), expect_success=False)
+
+
 def test_conflicting_writes() -> None:
     pathlib.Path("ginjarator.toml").write_text(
         textwrap.dedent(
