dseomn/ginjarator

commit 5c885111c3a830e8d17069e351b9a1760efd093f

Use relative paths in ninja files
Templates all use relative paths to call the ginjarator.fs API, so it
was a bit strange that ninja templates couldn't use relative paths too.
This should also make ninja files easier to read, and it gets rid of a
special case for build.ninja being relative when everything else was
absolute.

It does make the e2e tests slower. I haven't looked into that yet, but I
suspect that's because paths.Filesystem isn't as optimized as
pathlib.Path and pathlib.PurePath. Maybe going back to NewType would
make sense, or maybe using __slots__. Alternatively, the performance
difference might be trivial in real use when there are more templates
and each template does more.

ginjarator/build.py
@@ -13,9 +13,10 @@
 # limitations under the License.
 """Build system utilities."""
 
-import pathlib
 import shlex
 from typing import Any, Collection, Mapping
+
+from ginjarator import paths
 
 
 def to_ninja(value: Any, *, escape_shell: bool = False) -> str:
@@ -36,7 +37,7 @@ def to_ninja(value: Any, *, escape_shell: bool = False) -> str:
                     )
                 )
             )
-        case pathlib.Path():
+        case paths.Filesystem():
             return to_ninja(str(value), escape_shell=escape_shell)
         case list() | tuple():
             return " ".join(
@@ -50,7 +51,7 @@ def to_ninja(value: Any, *, escape_shell: bool = False) -> str:
             )
 
 
-def _depfile_escape(path: str | pathlib.Path) -> str:
+def _depfile_escape(path: str | paths.Filesystem) -> str:
     # The syntax does not seem to be well documented in any one place. The
     # Target Rules section of
     # https://pubs.opengroup.org/onlinepubs/9799919799/utilities/make.html
@@ -69,7 +70,9 @@ def _depfile_escape(path: str | pathlib.Path) -> str:
 
 
 def to_depfile(
-    dependency_map: Mapping[str | pathlib.Path, Collection[str | pathlib.Path]],
+    dependency_map: Mapping[
+        str | paths.Filesystem, Collection[str | paths.Filesystem]
+    ],
 ) -> str:
     """Returns depfile contents given a map from target to dependencies."""
     lines = []

ginjarator/init.py
@@ -25,23 +25,12 @@ from ginjarator import template
 
 _NINJA_REQUIRED_VERSION = "1.10"
 
-# It seems that build.ninja needs to be a relative path for ninja to reload it
-# properly when it changes, so this is used instead of fs.resolve() or other
-# things that use fs.resolve(), like fs.add_output().
-_NINJA_ENTRYPOINT = pathlib.Path(paths.NINJA_ENTRYPOINT)
 
-
-def _main_ninja_for_template(
-    template_name: paths.Filesystem,
-    *,
-    fs: filesystem.Filesystem,
-) -> str:
-    path = fs.resolve(template_name)
-    state_path = fs.resolve(paths.template_state(template_name))
-    depfile_path = fs.resolve(paths.template_depfile(template_name))
-    dyndep_path = fs.resolve(paths.template_dyndep(template_name))
-    render_stamp_path = fs.resolve(paths.template_render_stamp(template_name))
-    scan_done_stamp_path = fs.resolve(paths.SCAN_DONE_STAMP)
+def _main_ninja_for_template(template_name: paths.Filesystem) -> str:
+    state_path = paths.template_state(template_name)
+    depfile_path = paths.template_depfile(template_name)
+    dyndep_path = paths.template_dyndep(template_name)
+    render_stamp_path = paths.template_render_stamp(template_name)
     return textwrap.dedent(
         f"""\
         build $
@@ -51,9 +40,9 @@ def _main_ninja_for_template(
                 {build.to_ninja(dyndep_path)} $
                 : $
                 scan $
-                {build.to_ninja(path)} $
+                {build.to_ninja(template_name)} $
                 || $
-                {build.to_ninja(_NINJA_ENTRYPOINT)}
+                {build.to_ninja(paths.NINJA_ENTRYPOINT)}
             depfile = {build.to_ninja(depfile_path)}
             template = {build.to_ninja(str(template_name), escape_shell=True)}
 
@@ -61,12 +50,12 @@ def _main_ninja_for_template(
                 {build.to_ninja(render_stamp_path)} $
                 : $
                 render $
-                {build.to_ninja(path)} $
+                {build.to_ninja(template_name)} $
                 | $
                 {build.to_ninja(state_path)} $
                 || $
                 {build.to_ninja(dyndep_path)} $
-                {build.to_ninja(scan_done_stamp_path)}
+                {build.to_ninja(paths.SCAN_DONE_STAMP)}
             dyndep = {build.to_ninja(dyndep_path)}
             template = {build.to_ninja(str(template_name), escape_shell=True)}
         """
@@ -78,7 +67,6 @@ def _main_ninja(
     fs: filesystem.Filesystem,
     config_: config.Config,
 ) -> str:
-    scan_done_stamp_path = fs.resolve(paths.SCAN_DONE_STAMP)
     scan_done_dependencies = []
 
     parts = []
@@ -110,29 +98,25 @@ def _main_ninja(
     )
 
     for template_name in config_.templates:
-        parts.append(_main_ninja_for_template(template_name, fs=fs))
-        scan_done_dependencies.append(
-            fs.resolve(paths.template_state(template_name))
-        )
+        parts.append(_main_ninja_for_template(template_name))
+        scan_done_dependencies.append(paths.template_state(template_name))
 
-    depfile_path = fs.resolve(paths.NINJA_ENTRYPOINT_DEPFILE)
     fs.write_text(
         paths.NINJA_ENTRYPOINT_DEPFILE,
-        build.to_depfile({_NINJA_ENTRYPOINT: fs.dependencies}),
+        build.to_depfile({paths.NINJA_ENTRYPOINT: fs.dependencies}),
     )
 
     parts.append(
         textwrap.dedent(
             f"""\
             build $
-                    {build.to_ninja(_NINJA_ENTRYPOINT)} $
                     {build.to_ninja(sorted(fs.outputs))} $
                     : $
                     init
-                depfile = {build.to_ninja(depfile_path)}
+                depfile = {build.to_ninja(paths.NINJA_ENTRYPOINT_DEPFILE)}
 
             build $
-                    {build.to_ninja(scan_done_stamp_path)} $
+                    {build.to_ninja(paths.SCAN_DONE_STAMP)} $
                     : $
                     touch $
                     | $
@@ -177,7 +161,7 @@ def init(
 
     for template_name in config_.ninja_templates:
         template_ninja_path = paths.ninja_template_output(template_name)
-        subninjas.append(fs.resolve(template_ninja_path))
+        subninjas.append(template_ninja_path)
         subninjas_changed.append(
             fs.write_text(
                 template_ninja_path,
@@ -188,7 +172,8 @@ def init(
     # This has to be the last subninja, so that it can include the dependencies
     # and outputs added by previous subninjas.
     fs.add_output(paths.NINJA_MAIN)
-    subninjas.append(fs.resolve(paths.NINJA_MAIN))
+    fs.add_output(paths.NINJA_ENTRYPOINT)
+    subninjas.append(paths.NINJA_MAIN)
     subninjas_changed.append(
         fs.write_text(paths.NINJA_MAIN, _main_ninja(fs=fs, config_=config_))
     )

ginjarator/template.py
@@ -70,7 +70,7 @@ class _Loader(jinja2.BaseLoader):
             raise jinja2.TemplateNotFound(template) from e
         if contents is None:
             raise jinja2.TemplateNotFound(f"{template!r} is not built yet.")
-        return (contents, str(self._fs.resolve(template)), lambda: False)
+        return (contents, str(self._fs.root / template), lambda: False)
 
 
 def _render(api: Api, template_name: str) -> str:
@@ -92,10 +92,7 @@ def ninja(
 ) -> str:
     """Returns custom ninja from the given template."""
     api = Api(
-        fs=filesystem.Filesystem(
-            internal_fs.resolve("."),
-            mode=filesystem.NinjaMode(),
-        ),
+        fs=filesystem.Filesystem(internal_fs.root, mode=filesystem.NinjaMode()),
     )
     contents = _render(api, template_name)
     for dependency in api.fs.dependencies:
@@ -130,13 +127,9 @@ def scan(
     )
     internal_fs.write_text(
         paths.template_depfile(template_name),
-        build.to_depfile(
-            {internal_fs.resolve(state_path): api.fs.dependencies}
-        ),
+        build.to_depfile({state_path: api.fs.dependencies}),
     )
-    render_stamp_path = internal_fs.resolve(
-        paths.template_render_stamp(template_name)
-    )
+    render_stamp_path = paths.template_render_stamp(template_name)
     internal_fs.write_text(
         paths.template_dyndep(template_name),
         textwrap.dedent(
@@ -172,8 +165,10 @@ def render(
         fs=filesystem.Filesystem(
             root_path,
             mode=filesystem.RenderMode(
-                dependencies=tuple(map(pathlib.Path, state["dependencies"])),
-                outputs=tuple(map(pathlib.Path, state["outputs"])),
+                dependencies=tuple(
+                    map(paths.Filesystem, state["dependencies"])
+                ),
+                outputs=tuple(map(paths.Filesystem, state["outputs"])),
             ),
         ),
     )

ginjarator/build_test.py
@@ -14,13 +14,13 @@
 
 # pylint: disable=missing-module-docstring
 
-import pathlib
 import textwrap
 from typing import Any
 
 import pytest
 
 from ginjarator import build
+from ginjarator import paths
 
 
 @pytest.mark.parametrize(
@@ -28,7 +28,7 @@ from ginjarator import build
     (
         ("foo: $bar", True, "'foo$:$ $$bar'"),
         ("foo: $bar", False, "foo$:$ $$bar"),
-        (pathlib.Path("foo"), False, "foo"),
+        (paths.Filesystem("foo"), False, "foo"),
         (["foo", "bar"], False, "foo bar"),
         (("foo", "bar"), False, "foo bar"),
         ({"foo", "bar"}, False, "bar foo"),

ginjarator/e2e_test.py
@@ -80,11 +80,7 @@ def test_ninja_template() -> None:
             """\
             rule init
                 command = printf contents > $out
-            build $
-                    {{ ginjarator.to_ninja(
-                        ginjarator.fs.resolve("build/out")
-                    ) }}: $
-                    init
+            build build/out: init
             """
         )
     )
@@ -113,11 +109,7 @@ def test_add_ninja_template() -> None:
             """\
             rule write
                 command = printf contents > $out
-            build $
-                    {{ ginjarator.to_ninja(
-                        ginjarator.fs.resolve("build/out")
-                    ) }}: $
-                    write
+            build build/out: write
             """
         )
     )
@@ -142,11 +134,7 @@ def test_remove_ninja_template() -> None:
             """\
             rule write
                 command = printf contents > $out
-            build $
-                    {{ ginjarator.to_ninja(
-                        ginjarator.fs.resolve("build/out")
-                    ) }}: $
-                    write
+            build build/out: write
             """
         )
     )
@@ -189,11 +177,7 @@ def test_update_ninja_template_dependency() -> None:
             """\
             rule write
                 command = printf contents > $out
-            build $
-                    {{ ginjarator.to_ninja(
-                        ginjarator.fs.resolve("build/out")
-                    ) }}: $
-                    write
+            build build/out: write
             """
         )
     )

commit 0879cdec1fbf23e0aa4a9095d515e4da2c0ef4d0

Align depfiles with ninja's expectations
This also fixes the performance regression from 5c88511 (Use relative
paths in ninja files, 2025-06-14). I'm guessing that things were being
rebuilt more than needed, and I have an idea for how to test for that.

ginjarator/build.py
@@ -14,7 +14,7 @@
 """Build system utilities."""
 
 import shlex
-from typing import Any, Collection, Mapping
+from typing import Any, Collection
 
 from ginjarator import paths
 
@@ -70,15 +70,20 @@ def _depfile_escape(path: str | paths.Filesystem) -> str:
 
 
 def to_depfile(
-    dependency_map: Mapping[
-        str | paths.Filesystem, Collection[str | paths.Filesystem]
-    ],
+    *,
+    first_output: str | paths.Filesystem,
+    dependencies: Collection[str | paths.Filesystem],
 ) -> str:
-    """Returns depfile contents given a map from target to dependencies."""
-    lines = []
-    for target, dependencies in dependency_map.items():
-        for dependency in dependencies:
-            lines.append(
-                f"{_depfile_escape(target)}: {_depfile_escape(dependency)}\n"
-            )
-    return "".join(lines)
+    """Returns depfile contents.
+
+    Args:
+        first_output: The ninja build statement's first output. From
+            https://github.com/ninja-build/ninja/blob/1b52e21f4b0183ec5689da830b86300398dffc2a/src/graph.cc#L681-L690
+            it looks like this has to also be the first output in the depfile,
+            or ninja will ignore the entire depfile.
+        dependencies: The build statement's dependencies.
+    """
+    return "".join(
+        f"{_depfile_escape(first_output)}: {_depfile_escape(dependency)}\n"
+        for dependency in dependencies
+    )

ginjarator/init.py
@@ -101,13 +101,20 @@ def _main_ninja(
 
     fs.write_text(
         paths.NINJA_ENTRYPOINT_DEPFILE,
-        build.to_depfile({paths.NINJA_ENTRYPOINT: fs.dependencies}),
+        build.to_depfile(
+            first_output=paths.NINJA_ENTRYPOINT,
+            dependencies=fs.dependencies,
+        ),
     )
 
     parts.append(
         textwrap.dedent(
             f"""\
-            build {build.to_ninja(sorted(fs.outputs))}: init
+            build $
+                    {build.to_ninja(paths.NINJA_ENTRYPOINT)} $
+                    {build.to_ninja(sorted(fs.outputs))} $
+                    : $
+                    init
                 depfile = {build.to_ninja(paths.NINJA_ENTRYPOINT_DEPFILE)}
 
             build $
@@ -167,7 +174,6 @@ def init(
     # This has to be the last subninja, so that it can include the dependencies
     # and outputs added by previous subninjas.
     fs.add_output(paths.NINJA_MAIN)
-    fs.add_output(paths.NINJA_ENTRYPOINT)
     subninjas.append(paths.NINJA_MAIN)
     subninjas_changed.append(
         fs.write_text(paths.NINJA_MAIN, _main_ninja(fs=fs, config_=config_))

ginjarator/template.py
@@ -130,7 +130,10 @@ def scan(
     )
     internal_fs.write_text(
         paths.template_depfile(template_name),
-        build.to_depfile({state_path: scan_dependencies}),
+        build.to_depfile(
+            first_output=state_path,
+            dependencies=scan_dependencies,
+        ),
     )
     render_stamp_path = paths.template_render_stamp(template_name)
     internal_fs.write_text(

ginjarator/build_test.py
@@ -64,20 +64,17 @@ def test_to_ninja_error(value: Any) -> None:
 )
 def test_to_depfile_error(path: str) -> None:
     with pytest.raises(NotImplementedError, match="Unsupported characters"):
-        build.to_depfile({path: ("foo",)})
+        build.to_depfile(first_output=path, dependencies=("foo",))
 
 
 def test_to_depfile() -> None:
     assert build.to_depfile(
-        {
-            "t1": ("d1", "d2"),
-            "t2": ("d3",),
-        }
+        first_output="t1",
+        dependencies=("d1", "d2"),
     ) == textwrap.dedent(
         """\
         t1: d1
         t1: d2
-        t2: d3
         """
     )
 
@@ -92,11 +89,14 @@ def test_to_depfile_ninja_requires_depfile_outputs_to_be_known(
     (tmp_path / "depfile").write_text(
         "\n".join(
             (
-                # The first output in the depfile must be the same as the first
-                # output in the build statement, otherwise ninja gives a warning
-                # but continues.
-                build.to_depfile({"output": ("input",)}),
-                build.to_depfile({"unknown-kumquat": ("input",)}),
+                build.to_depfile(
+                    first_output="output",
+                    dependencies=("input",),
+                ),
+                build.to_depfile(
+                    first_output="unknown-kumquat",
+                    dependencies=("input",),
+                ),
             )
         )
     )
@@ -129,12 +129,15 @@ def test_to_depfile_escaping_works_with_ninja(tmp_path: pathlib.Path) -> None:
     (tmp_path / "depfile").write_text(
         "\n".join(
             (
-                # The first output in the depfile must be the same as the first
-                # output in the build statement, otherwise ninja gives a warning
-                # but continues.
-                build.to_depfile({"output": ("input",)}),
+                build.to_depfile(
+                    first_output="output",
+                    dependencies=("input",),
+                ),
                 *(
-                    build.to_depfile({filename: ("input",)})
+                    build.to_depfile(
+                        first_output=filename,
+                        dependencies=("input",),
+                    )
                     for filename in filenames_to_test
                 ),
             )